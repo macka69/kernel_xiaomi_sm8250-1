ddb7c10b6d0e11afcc4167edbaf4fa422b40e590
John Macka <macka2004@gmail.com>
2025-08-20 09:31:17 +1000

    treewide: Import susfs v1.5.7 patches
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1546,6 +1546,9 @@ struct package_runtime_info pkg;
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
+#ifdef CONFIG_KSU_SUSFS
+	u64 susfs_task_state;
+#endif
 	randomized_struct_fields_end
 
 	/* CPU-specific state of this task: */
